e4084cec13f26c68c35d40dffe3a6a4201002ffe
Alessandro <nathannemesis2@gmail.com>
2023-09-21 15:56:06 +0200

    servizio

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { DemoSongs, Song } from '../models/song.models';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { IPlayer } from '../models/player.models';
 
 @Injectable({
@@ -20,5 +20,37 @@ export class PlayerService {
       currentSong: this.currentSong,
       songs: this.songs,
     });
+  }
+  getPlayer$(): Observable<IPlayer> {
+    return this.player$.asObservable();
+  }
+
+  private update() {
+    this.player$.next({ currentSong: this.currentSong, songs: this.songs });
+  }
+
+  next(){
+    if(this.songs.length && this.currentSong){
+      const posizione = this.songs.indexOf(this.currentSong);
+      const next = posizione < (this.songs.length-1) ? posizione+1 : 0;
+      this.currentSong = this.songs[next];
+      this.update();
+    }else return;
+  }
+
+  prev(){
+    if(this.songs.length && this.currentSong){
+      const posizione = this.songs.indexOf(this.currentSong);
+      const prev = posizione > 0 ? posizione-1 : (this.songs.length-1) ;
+      this.currentSong = this.songs[prev];
+      this.update();
+    }else return;
+  }
+  
+  setPicture(i:number){
+    if(i <= (this.songs.length - 1)){
+      this.currentSong = this.songs[i];
+      this.update();
+    }
   }
 }
