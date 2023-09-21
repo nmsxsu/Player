ec00151d1b918cb4f785c9a3bbccc2714d62532b
Flora Esposito <flora.98e@gmail.com>
2023-09-21 15:48:13 +0200

    deleted unused imports from app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PlayerComponent } from './player/player.component';
-import { HeaderComponent } from './player/header/header.component';
-import { SongImageComponent } from './player/song-image/song-image.component';
-import { ActionsComponent } from './player/actions/actions.component';
-import { SongListComponent } from './player/song-list/song-list.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { PlayerModule } from './player/player.module';
 
